40fc69d00a61cbdc169cf12251d59270bd3ee826
rwarren1333 <rwarren@americanprogress.org>
2016-03-30 16:15:06 -0400

    updated to version 3.9.4

--- class.jetpack-autoupdate.php
+++ class.jetpack-autoupdate.php
@@ -6,17 +6,16 @@
  */
 class Jetpack_Autoupdate {
 
-	public $updates_allowed;
-	public $jetpack;
-	public $autoupdate_results;
-	public $is_updating = false;
+	private $results = array();
 
-	public $autoupdate_expected = array(
-		'plugin'=> array(),
+	private $expected = array();
+
+	private $success = array(
+		'plugin' => array(),
 		'theme' => array(),
 	);
 
-	public $log = array(
+	private $failed = array(
 		'plugin' => array(),
 		'theme' => array(),
 	);
@@ -31,52 +30,33 @@ class Jetpack_Autoupdate {
 	}
 
 	private function __construct() {
-		$this->updates_allowed = Jetpack::is_module_active( 'manage' );
-
-		// Only run automatic updates if a user as opted in by activating the manage module.
-		if ( $this->updates_allowed ) {
+		if ( Jetpack::is_module_active( 'manage' ) ) {
 			add_filter( 'auto_update_plugin',  array( $this, 'autoupdate_plugin' ), 10, 2 );
 			add_filter( 'auto_update_theme',   array( $this, 'autoupdate_theme' ), 10, 2 );
 			add_filter( 'auto_update_core',    array( $this, 'autoupdate_core' ), 10, 2 );
-			add_action( 'automatic_updates_complete', array( $this, 'automatic_updates_complete' ), 10, 1 );
-			add_action( 'shutdown', array( $this, 'log_results' ) );
+			add_action( 'automatic_updates_complete', array( $this, 'automatic_updates_complete' ), 999, 1 );
 		}
-
-		if ( is_main_site() ) {
-			// Anytime WordPress saves update data, we'll want to update our Jetpack option as well.
-			add_action( 'set_site_transient_update_plugins', array( $this, 'save_update_data' ) );
-			add_action( 'set_site_transient_update_themes', array( $this, 'save_update_data' ) );
-			add_action( 'set_site_transient_update_core', array( $this, 'save_update_data' ) );
-
-			// Anytime a connection to jetpack is made, sync the update data
-			add_action( 'jetpack_site_registered', array( $this, 'save_update_data' ) );
-
-			// Anytime the Jetpack Version changes, sync the the update data
-			add_action( 'updating_jetpack_version', array( $this, 'save_update_data' ) );
-		}
-
 	}
 
-	function autoupdate_plugin( $update, $item ) {
+	public function autoupdate_plugin( $update, $item ) {
 		$autoupdate_plugin_list = Jetpack_Options::get_option( 'autoupdate_plugins', array() );
 		if ( in_array( $item->plugin, $autoupdate_plugin_list ) ) {
-			$this->expect( $item->plugin );
+			$this->expect( $item->plugin, 'plugin' );
  			return true;
 		}
-
 		return $update;
 	}
 
-	function autoupdate_theme( $update, $item ) {
+	public function autoupdate_theme( $update, $item ) {
 		$autoupdate_theme_list = Jetpack_Options::get_option( 'autoupdate_themes', array() );
 		if ( in_array( $item->theme , $autoupdate_theme_list) ) {
-			$this->expect( $item->theme, $type = 'theme' );
+			$this->expect( $item->theme, 'theme' );
 			return true;
 		}
 		return $update;
 	}
 
-	function autoupdate_core( $update, $item ) {
+	public function autoupdate_core( $update, $item ) {
 		$autoupdate_core = Jetpack_Options::get_option( 'autoupdate_core', false );
 		if ( $autoupdate_core ) {
 			return $autoupdate_core;
@@ -85,71 +65,16 @@ class Jetpack_Autoupdate {
 	}
 
 	/**
-	 * Stores the an item identifier to the autoupdate_expected array.
+	 * Stores the an item identifier to the expected array.
 	 *
 	 * @param string $item  Example: 'jetpack/jetpack.php' for type 'plugin' or 'twentyfifteen' for type 'theme'
 	 * @param string $type 'plugin' or 'theme'
 	 */
-	function expect( $item, $type='plugin' ) {
-		$this->is_updating = true;
-		$this->autoupdate_expected[ $type ][] = $item;
-	}
-
-	/**
-	 * Calculates available updates and saves them to a Jetpack Option
-	 *
-	 * jetpack_updates is saved in the following schema:
-	 *
-	 * array (
-	 *      'plugins'                       => (int) Number of plugin updates available.
-	 *      'themes'                        => (int) Number of theme updates available.
-	 *      'wordpress'                     => (int) Number of WordPress core updates available.
-	 *      'translations'                  => (int) Number of translation updates available.
-	 *      'total'                         => (int) Total of all available updates.
-	 *      'wp_update_version'             => (string) The latest available version of WordPress, only present if a WordPress update is needed.
-	 * )
-	 *
-	 * jetpack_update_details is saved in the following schema:
-	 *
-	 * array (
-	 *      'update_core'       => (array) The contents of the update_core transient.
-	 *      'update_themes'     => (array) The contents of the update_themes transient.
-	 *      'update_plugins'    => (array) The contents of the update_plugins transient.
-	 * )
-	 *
-	 */
-	function save_update_data() {
-
-		if ( ! current_user_can( 'update_plugins' ) || ! current_user_can( 'update_core') || ! current_user_can( 'update_themes') ) {
-			// `wp_get_updated_data` will not return useful information if a user does not have the capabilities.
-			// We should should therefore bail to avoid saving incomplete data.
-			return;
+	private function expect( $item, $type ) {
+		if ( ! isset( $this->expected[ $type ] ) ) {
+			$this->expected[ $type ] = array();
 		}
-
-		$update_data = wp_get_update_data();
-
-		// Stores the individual update counts as well as the total count.
-		if ( isset( $update_data['counts'] ) ) {
-			$updates = $update_data['counts'];
-		}
-
-		// If we need to update WordPress core, let's find the latest version number.
-		if ( ! empty( $updates['wordpress'] ) ) {
-			$cur = get_preferred_from_update_core();
-			if ( isset( $cur->response ) && 'upgrade' === $cur->response ) {
-				$updates['wp_update_version'] = $cur->current;
-			}
-		}
-
-		Jetpack_Options::update_option( 'updates', $updates );
-
-		// Let's also store and sync more details about what updates are needed.
-		$update_details = array(
-			'update_core' => get_site_transient( 'update_core' ),
-			'update_plugins' => get_site_transient( 'update_plugins' ),
-			'update_themes' => get_site_transient( 'update_themes' ),
-		);
-		Jetpack_Options::update_option( 'update_details', $update_details );
+		$this->expected[ $type ][] = $item;
 	}
 
 	/**
@@ -157,30 +82,30 @@ class Jetpack_Autoupdate {
 	 *
 	 * @param $results - Sent by WP_Automatic_Updater after it completes an autoupdate action. Results may be empty.
 	 */
-	function automatic_updates_complete( $results ) {
-		$this->autoupdate_results = $results;
+	public function automatic_updates_complete( $results ) {
+		if ( empty( $this->expected ) ) {
+			return;
+		}
+		$this->results = empty( $results ) ? self::get_possible_failures() : $results;
+
+		add_action( 'shutdown', array( $this, 'bump_stats' ) );
+
+		Jetpack::init();
+
+		$items_to_log = array( 'plugin', 'theme' );
+		foreach( $items_to_log as $items ) {
+			$this->log_items( $items );
+		}
+
+		Jetpack::log( 'autoupdates', $this->get_log() );
 	}
 
-	/**
-	 * On shutdown, let's check to see if we've preformed an automatic update.
-	 * If so, let's compare the expected results to the actual results, and log our findings.
-	 *
-	 * Results are logged locally via Jetpack::log(), and globally via Jetpack::do_stats()
-	 */
-	function log_results() {
-
-		if ( $this->is_updating ) {
-
-			$this->jetpack = Jetpack::init();
-			$items_to_log = array( 'plugin', 'theme' );
-
-			foreach( $items_to_log as $items ) {
-				$this->log_items( $items );
-			}
-
-			$this->jetpack->do_stats( 'server_side' );
-			$this->jetpack->log( 'autoupdates', $this->log );
-		}
+	public function get_log() {
+		return array(
+			'results'	=> $this->results,
+			'failed'	=> $this->failed,
+			'success'	=> $this->success
+		);
 	}
 
 	/**
@@ -188,41 +113,60 @@ class Jetpack_Autoupdate {
 	 *
 	 * @param $items 'plugin' or 'theme'
 	 */
-	function log_items( $items ) {
-		$num_items_updated = 0;
-		$num_items_failed  = 0;
-		$item_results      = $this->get_successful_updates( $items );
-		$items_failed      = array();
+	private function log_items( $items ) {
 
-		foreach( $this->autoupdate_expected[ $items ] as $item ) {
-			if ( in_array( $item, $item_results ) ) {
-				$num_items_updated++;
-				$this->log[ $items ][ $item ] = true;
-			} else {
-				$num_items_failed++;
-				$this->log[ $items ][ $item ] = new WP_Error( "$items-fail", $this->get_error_message( $item, $type = $items ) );
-				$items_failed[] = $item;
+		if ( ! isset( $this->expected[ $items ] ) ) {
+			return;
+		}
+
+		$item_results = $this->get_successful_updates( $items );
+
+		if ( is_array( $this->expected[ $items ] ) ) {
+			foreach( $this->expected[ $items ] as $item ) {
+				if ( in_array( $item, $item_results ) ) {
+						$this->success[ $items ][] = $item;
+				} else {
+						$this->failed[ $items ][] = $item;
+				}
 			}
 		}
+	}
 
-		if ( $num_items_updated ) {
-			$this->jetpack->stat( "autoupdates/$items-success", $num_items_updated );
+	public function bump_stats() {
+		$instance = Jetpack::init();
+		$log = array();
+		// Bump numbers
+		if ( ! empty( $this->success['plugin'] ) ) {
+			$instance->stat( 'autoupdates/plugin-success', count( $this->success['plugin'] ) );
+			$log['plugins_success'] = $this->success['plugin'];
 		}
 
-		if ( $num_items_failed ) {
-			// bump stats
-			$this->jetpack->stat( "autoupdates/$items-fail", $num_items_failed );
+		if ( ! empty( $this->failed['plugin'] ) ) {
+			$instance->stat( 'autoupdates/plugin-fail', count( $this->failed['plugin'] ) );
+			$log['plugins_failed'] = $this->failed['plugin'];
+		}
+
+		if ( ! empty( $this->success['theme'] ) ) {
+			$instance->stat( 'autoupdates/theme-success', count( $this->success['theme'] ) );
+			$log['themes_success'] = $this->success['theme'];
+		}
+
+		if ( ! empty( $this->failed['theme'] ) ) {
+			$instance->stat( 'autoupdates/theme-fail', count( $this->failed['theme'] ) );
+			$log['themes_failed'] = $this->failed['theme'];
+		}
+
+		$instance->do_stats( 'server_side' );
+
+		// Send a more detailed log to logstash
+		if ( ! empty( $log ) ) {
 			Jetpack::load_xml_rpc_client();
 			$xml = new Jetpack_IXR_Client( array(
 				'user_id' => get_current_user_id()
 			) );
-			$request = array(
-				'plugins' => $items_failed,
-				'blog_id' => Jetpack_Options::get_option( 'id' ),
-			);
-			$xml->query( 'jetpack.debug_autoupdate', $request );
+			$log['blog_id'] = Jetpack_Options::get_option( 'id' );
+			$xml->query( 'jetpack.debug_autoupdate', $log );
 		}
-
 	}
 
 	/**
@@ -232,20 +176,20 @@ class Jetpack_Autoupdate {
 	 *
 	 * @return array
 	 */
-	private function get_successful_updates( $type = 'plugin' ) {
+	private function get_successful_updates( $type ) {
 		$successful_updates = array();
 
-		if ( ! isset( $this->autoupdate_results[ $type ] ) ) {
+		if ( ! isset( $this->results[ $type ] ) ) {
 			return $successful_updates;
 		}
 
-		foreach( $this->autoupdate_results[ $type ] as $result ) {
+		foreach( $this->results[ $type ] as $result ) {
 			if ( $result->result ) {
 				switch( $type ) {
 					case 'theme':
 						$successful_updates[] = $result->item->theme;
 						break;
-					default:
+					case 'plugin':
 						$successful_updates[] = $result->item->plugin;
 				}
 			}
@@ -254,31 +198,48 @@ class Jetpack_Autoupdate {
 		return $successful_updates;
 	}
 
-	/**
-	 * Cycles through results generated by WP_Automatic_Updater to find the messages for the given item and item type.
-	 *
-	 * @param $item Example: 'jetpack/jetpack.php' for type 'plugin' or 'twentyfifteen' for type 'theme'
-	 * @param string $type 'plugin' or 'theme'
-	 *
-	 * @return bool|string
-	 */
-	private function get_error_message( $item, $type = 'plugin' ) {
-		if ( ! isset( $this->autoupdate_results[ $type ] ) ) {
-			return false;
+	static function get_possible_failures() {
+		$result = array();
+		// Lets check some reasons why it might not be working as expected
+		include_once( ABSPATH . '/wp-admin/includes/admin.php' );
+		include_once( ABSPATH . '/wp-admin/includes/class-wp-upgrader.php' );
+		$upgrader = new WP_Automatic_Updater;
+
+		if ( $upgrader->is_disabled() ) {
+			$result[] = 'autoupdates-disabled';
 		}
-		foreach( $this->autoupdate_results[ $type ] as $result ) {
-			switch( $type ) {
-				case 'theme':
-					$id = $result->item->theme;
-					break;
-				default:
-					$id = $result->item->plugin;
-			}
-			if ( $id == $item && isset( $result->messages ) ) {
-				return implode( ', ', $result->messages );
-			}
+		if ( ! is_main_site() ) {
+			$result[] = 'is-not-main-site';
 		}
-		return false;
+		if ( ! is_main_network() ) {
+			$result[] = 'is-not-main-network';
+		}
+		if ( $upgrader->is_vcs_checkout( ABSPATH ) ) {
+			$result[] = 'site-on-vcs';
+		}
+		if ( $upgrader->is_vcs_checkout( WP_PLUGIN_DIR ) ) {
+			$result[] = 'plugin-directory-on-vcs';
+		}
+		if ( $upgrader->is_vcs_checkout( WP_CONTENT_DIR ) ) {
+			$result[] = 'content-directory-on-vcs';
+		}
+		$lock = get_option( 'auto_updater.lock' );
+		if ( $lock > ( time() - HOUR_IN_SECONDS ) ) {
+			$result[] = 'lock-is-set';
+		}
+		$skin = new Automatic_Upgrader_Skin;
+		include_once( ABSPATH . 'wp-admin/includes/file.php' );
+		include_once( ABSPATH . 'wp-admin/includes/template.php' );
+		if ( ! $skin->request_filesystem_credentials( false, ABSPATH, false ) ) {
+			$result[] = 'no-system-write-access';
+		}
+		if ( ! $skin->request_filesystem_credentials( false, WP_PLUGIN_DIR, false )  ) {
+			$result[] = 'no-plugin-directory-write-access';
+		}
+		if ( ! $skin->request_filesystem_credentials( false,  WP_CONTENT_DIR, false ) ) {
+			$result[] = 'no-wp-content-directory-write-access';
+		}
+		return $result;
 	}
 
 }
